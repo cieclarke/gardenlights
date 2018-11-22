dd7539f1fac6369e04fbd8e8122ffd02cf41acb3
cieclarke <chris@cieclarke.com>
2018-11-22 12:24:42 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const client = require('./client');
+const readline = require('readline');
+
+var currentOption = 'm';
+const menu = 'm - this menu 0 - close 1 - all on 2 - all off\n';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  
+
+  var q = () => { rl.question('option:', (answer) => {
+    currentOption = answer;
+
+    switch(currentOption) {
+        case '0' : rl.close(); process.exit();
+            break;
+        case '1' : client.On("*"); q();
+            break;
+        case '2' : client.Off("*"); q();
+            break;
+        default : break;
+
+    }
+
+  });
+
+}
+
+console.log(menu);
+q();
+
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const client = require('./client');
+var path = require('path');
+
+app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.htm'));
+})
+
+app.post('/all/on', function (req, res) {
+    res.json({all : 'on'});
+    client.On("*"); 
+})
+
+app.post('/all/off', function (req, res) {
+    res.json({all: 'off'});
+    client.Off("*"); 
+})
+
+app.post('/:zone/on', function (req, res) {
+    res.json({zone: req.params.zone + ' on'});
+    client.On(req.params.zone); 
+})
+
+app.post('/:zone/off', function (req, res) {
+    res.json({all: req.params.zone + ' off'});
+    client.Off(req.params.zone); 
+})
+
+app.listen(3000, () => console.log(`Example app listening on port 3000`))
