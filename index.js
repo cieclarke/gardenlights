c035bc45429cf6167204afb1ab558a767f55c206
cieclarke <chris@cieclarke.com>
2018-12-04 16:15:59 +0000

    refactor webserver request functions

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const client = require('./client');
+const options = require('./options.json');
+const sender = require('./sender');
 const readline = require('readline');
 
 var currentOption = 'm';
-const menu = 'm - this menu 0 - close 1 - all on 2 - all off\n';
+const menu = '\nm - this menu\nx - close\n1 - all on\n2 - all off\n';
 
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-var q = () => { rl.question('option:', (answer) => {
-	currentOption = answer;
+var q = () => {
+	
+	rl.question('option:', (answer) => {
+		currentOption = answer;
 
-	switch(currentOption) {
-	case '0' : rl.close(); process.exit();
-		break;
-	case '1' : client.On('0'); q();
-		break;
-	case '2' : client.Off('0'); q();
-		break;
-	default : break;
+		switch(currentOption) {
+		case 'm' : console.log(menu); q();
+			break;
+		case 'x' : rl.close(); process.exit();
+			break;
+		case '1' : sender(currentOption, 'on', options.lsdevice.port, options.lsdevice.server); q();
+			break;
+		case '2' : sender(currentOption, 'off', options.lsdevice.port, options.lsdevice.server); q();
+			break;
+		default : break;
 
-	}
+		}
 
-});
+	});
 
 };
 
--- webserver.js
+++ webserver.js
@@ -1,23 +1,50 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const sender = require('./sender');
 
 module.exports = (filePath, port, lsport, lsserver) => {
 
 	const app = express();
-
+	app.use(bodyParser.json());
+	
 	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname + '/' + filePath));
 	});
 
-	app.post('/:zone/on', function (req, res) {
-		res.json({zone: req.params.zone + ' on'});
-		sender(req.params.zone, 'on', lsport, lsserver);
+	app.get('/readme', (req, res) => {
+		res.sendFile(path.join(__dirname + '/readme.md'));
 	});
 
-	app.post('/:zone/off', function (req, res) {
-		res.json({all: req.params.zone + ' off'});
-		sender(req.params.zone, 'off', lsport, lsserver);
+	app.post('/', (req, res) => {
+
+		console.log(req.body);
+		var reqBody = req.body;
+		reqBody.readme = req.protocol + '://' + req.hostname + ':' + port + '/readme';
+
+		if (!req.body) {
+			res.status(400);
+			res.type('application/json');
+
+			return res.send({
+				readme : reqBody.readme
+			});
+		}
+		if (req.body.command == undefined || req.body.zone == undefined) {
+			res.status(400);
+			res.type('application/json');
+
+			return res.send({
+				command : req.body.command == undefined ? 'undefined' : req.body.command,
+				zone : req.body.zone == undefined ? 'undefined' : req.body.zone,
+				readme : reqBody.readme
+			});
+		}
+		
+		
+		res.send(req.body);
+		
+		sender(req.body.zone, req.body.command, lsport, lsserver);
 	});
 
 	app.listen(port, () => console.log('Example app listening on port ' + port));
